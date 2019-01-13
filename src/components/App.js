6f4c998256e83ddb37a595cee8ac60e58104469f
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2019-01-13 13:36:25 -0500

    created video list for rendering videos and videoitem

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import yotube from '../api/yotube';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 
 export default class App extends Component {
   state = {
     videos: [],
+    selectedVideo: null,
   };
 
-  onSubmit = searchText => {
-    console.log(searchText);
+  onSubmit = async searchText => {
+    const response = await yotube.get('/search', {
+      params: { q: searchText },
+    });
+    const videos = response.data.items;
+    const selectedVideo = response.data.items[0];
+    this.setState({ videos, selectedVideo });
   };
 
   render() {
-    const { videos } = this.state;
+    const { videos, selectedVideo } = this.state;
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSubmit} />
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
+// import './VideoItem.css';
 
-const VideoList = ({ videos }) => (
-  <div>{videos.map(el => console.log(el))}</div>
-);
+const VideoList = ({ videos }) => {
+  const renderList = videos.map(video => (
+    <VideoItem key={video.id.videoId} video={video} />
+  ));
+  return <div className="ui relaxed divided list">{renderList}</div>;
+};
 
 export default VideoList;
